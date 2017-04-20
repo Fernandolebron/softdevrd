f907759bf8ff26b41537f5d54b71dac9c0ac2f60
Fernandolebron <fernandolebron616@gmail.com>
2017-04-20 16:49:37 -0400

    New Changes

---
+++ wp-content/themes/Softdevrd/footer.php
@@ -0,0 +1,46 @@
+<button onclick="topFunction()" id="myBtn" title="Go to top">
+	<i class="fa fa-sort-asc fa-5x" aria-hidden="true"></i>
+
+</button>
+
+<footer class="blog-footer">
+   
+      <!--</section>-->
+      <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?> </p>
+
+      <p>Todos los derechos reservados  by <a href="https://softdevrd.com">SoftDev</a>.</p></p>
+      
+    </footer>
+    <?php wp_footer();?>
+  <!-- jQuery -->
+    <script src="<?php echo get_template_directory_uri(); ?> /js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo get_template_directory_uri(); ?> /js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="<?php echo get_template_directory_uri(); ?> /js/jquery.easing.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?> /js/scrolling-nav.js"></script>
+
+    <script>
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
+
+</body>
+
+</html>
